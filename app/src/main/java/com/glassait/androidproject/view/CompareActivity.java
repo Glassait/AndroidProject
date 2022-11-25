054abc25c0503f25ca5cbb80df2cf7ee352fbbdc
Barbara Furkert <barbara.furkert@ges.thm.de>
2022-11-25 13:49:57 +0100

    chart

--- app/src/main/java/com/glassait/androidproject/view/CompareActivity.java
+++ app/src/main/java/com/glassait/androidproject/view/CompareActivity.java
@@ -4,9 +4,16 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
 import com.glassait.androidproject.R;
 import com.glassait.androidproject.common.utils.LocalData;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class CompareActivity extends AppCompatActivity {
@@ -18,8 +25,20 @@ public class CompareActivity extends AppCompatActivity {
 
         Map<Integer, String[]> database = LocalData.getInstance()
                                                    .getDatabase();
+        /**
         System.out.println("RESULT");
         System.out.println(LocalData.getInstance()
-                                    .getScanningCode());
+                                    .getScanningCode());*/
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("John", 10000));
+        data.add(new ValueDataEntry("Jake", 12000));
+        data.add(new ValueDataEntry("Peter", 18000));
+
+        pie.data(data);
+
+        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
+        anyChartView.setChart(pie);
     }
 }
